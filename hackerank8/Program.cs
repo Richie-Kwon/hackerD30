357074e571371bab6c4185b6671c803f5f81f896
richie <richie.kwon1@gmail.com>
2021-07-10 17:24:34 +0100

    Day8

---
+++ hackerank8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace hackerank8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entryOfEntry = Convert.ToInt32(Console.ReadLine());
+            IDictionary<string, string> phonebook = new Dictionary<string, string>();
+
+            for (int i = 0; i < entryOfEntry; i++)
+            {
+                var keyValues = Console.ReadLine().Split(' ');
+                phonebook.Add(keyValues[0], keyValues[1]);
+            }
+
+            var queryList = new List<string>();
+            for (int j = 0; j < entryOfEntry; j++)
+            {
+                queryList.Add(Console.ReadLine());
+            }
+
+            foreach (string k in queryList)
+            {
+                if (phonebook.ContainsKey(k))
+                {
+                    Console.WriteLine("{0}={1}", k, phonebook[k]);
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+            }
+        }
+    }
+}
+
---
+++ hakerank9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace hakerank9
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = factorial(n);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+        public static int factorial(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i<= n; i++)
+            {
+                fact = fact * i;
+            }
+            // Console.WriteLine(fact);
+
+            return fact;
+        }
+    }
+}
