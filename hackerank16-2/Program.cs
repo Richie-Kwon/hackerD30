99e0e521feaf79f198b5545260757ad5555d36fb
richie <richie.kwon1@gmail.com>
2021-07-22 20:34:45 +0100

    day16

---
+++ hackerank16-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace hackerank16_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+        
+            try{
+                var data = int.Parse(S);   
+                Console.WriteLine(data);  
+                
+            }    
+            catch{
+                Console.WriteLine("Bad String");
+        }
+    }
+}
