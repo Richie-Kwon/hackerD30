0a2bbbf11b8738fa13d2ea231a1c572250a60248
richie <richie.kwon1@gmail.com>
2021-07-30 22:27:50 +0100

    day6

---
+++ hackerankday6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace hackerankday6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var runCount = Convert.ToInt32(Console.ReadLine());
+            for (int k = 0; k < runCount; k++)
+            {
+                var input = Console.ReadLine();
+                var oddChars = new StringBuilder();
+                var evenChars = new StringBuilder();
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        oddChars.Append(input[i]);
+                    }
+                    else
+                    {
+                        evenChars.Append(input[i]);
+                    }
+                }
+                Console.WriteLine($"{evenChars} {oddChars}");   
+            }
+        }
+    }
+}
