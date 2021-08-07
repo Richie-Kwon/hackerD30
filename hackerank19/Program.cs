83d9e83e65a9ea3a3dd81913310c4c6bbc3e730d
richie <richie.kwon1@gmail.com>
2021-08-07 20:49:49 +0100

    Day19

---
+++ hackerank19/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace hackerank19
+{
+    public int divisorSum(int n)
+    {
+    var divisor = 0;
+    var share = 0;
+    var totalSum = 0;
+        while (divisor + 1 <= n / (divisor + 1))
+    {
+        divisor += 1;
+        if (n % divisor != 0) continue;
+
+        share = n / divisor;
+        totalSum += (divisor == share) ? divisor : divisor + share;
+    }
+    return totalSum;
+        
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
