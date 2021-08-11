f5901be23812ce82f51e3c5b0110071778d0e607
richie <richie.kwon1@gmail.com>
2021-08-11 21:45:26 +0100

    day20

--- hackerank19/Program.cs
+++ hackerank19/Program.cs
@@ -6,23 +6,18 @@ namespace hackerank19
 {
     public int divisorSum(int n)
     {
-    var divisor = 0;
-    var share = 0;
     var totalSum = 0;
-        while (divisor + 1 <= n / (divisor + 1))
+    for(int i =1; i == n ; i++)
     {
-        divisor += 1;
-        if (n % divisor != 0) continue;
-
-        share = n / divisor;
-        totalSum += (divisor == share) ? divisor : divisor + share;
+        if (n % i == 0) {
+            totalSum+=i;
+        }
+        else{
+        }
+               
     }
     return totalSum;
         
-}
-
-    class Program
-    {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
---
+++ hackerank20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace hackerank20
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+            
+            
+
+            // Write your code here
+            var temp = a[0];
+            var sCount =0;
+        
+            for (int i=0; i< n; i++){
+                for (int j = i+1; j < n-1 ; j++) {
+                    if (a[i] > a[j]){
+                        sCount = sCount +1;
+                        temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;                 
+                    
+                    }
+                }
+            }
+            Console.WriteLine("Array is sorted in {0} swaps.",sCount);
+            Console.WriteLine("First Element:{0}", a[0]);
+            Console.WriteLine("Last Element:{0}", a[-1]);
+        
+        }
+    }
+}
