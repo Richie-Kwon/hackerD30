d916601283d84207fe83f6af64570658a76f6238
richie <richie.kwon1@gmail.com>
2021-08-11 21:54:39 +0100

    bug fixed

--- hackerank20/Program.cs
+++ hackerank20/Program.cs
@@ -11,15 +11,16 @@ namespace hackerank20
             int n = Convert.ToInt32(Console.ReadLine().Trim());
 
             List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
-            
-            
 
             // Write your code here
             var temp = a[0];
             var sCount =0;
         
             for (int i=0; i< n; i++){
-                for (int j = i+1; j < n-1 ; j++) {
+                if (i== n){
+                    break;
+                }
+                for (int j = i+1; j < n ; j++) {
                     if (a[i] > a[j]){
                         sCount = sCount +1;
                         temp = a[i];
@@ -30,8 +31,8 @@ namespace hackerank20
                 }
             }
             Console.WriteLine("Array is sorted in {0} swaps.",sCount);
-            Console.WriteLine("First Element:{0}", a[0]);
-            Console.WriteLine("Last Element:{0}", a[-1]);
+            Console.WriteLine("First Element: {0}", a[0]);
+            Console.WriteLine("Last Element: {0}", a[n-1]);
         
         }
     }
