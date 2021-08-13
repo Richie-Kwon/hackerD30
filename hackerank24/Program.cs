7917d62aa26467cf030c909cd58b0a2f23f6dbb5
richie <richie.kwon1@gmail.com>
2021-08-13 21:17:51 +0100

    day24

---
+++ hackerank24/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace hackerank24
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d){
+            data=d;
+            next=null;
+        }
+		
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
