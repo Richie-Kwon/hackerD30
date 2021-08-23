3ddcb7161e5c0cd84fe9ef9aba808f2c7553f5c1
richie <richie.kwon1@gmail.com>
2021-08-23 21:34:19 +0100

    day25

---
+++ hackerank25/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace hackerank25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                bool prime = false;
+                if (n == 2)
+                {
+                    prime = true;
+                }
+                else
+                {
+
+                    for (int j = 2; j < n / 2 + 1; j++)
+                    {
+                        if (n % j == 0)
+                        {
+                            prime = false;
+                            break;
+
+                        }
+
+                        prime = true;
+                    }
+
+                }
+
+
+
+                if (prime)
+                {
+                    Console.WriteLine("Prime");
+                }
+                else
+                {
+                    Console.WriteLine("Not prime");
+                }
+            }
+        }
+    }
+}
