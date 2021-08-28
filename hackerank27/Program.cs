7a424164fd64a4dce523cc74e700662b11823987
richie <richie.kwon1@gmail.com>
2021-08-28 20:25:50 +0100

    day29

---
+++ hackerank27/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hackerank27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
